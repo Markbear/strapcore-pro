cd4a0fea906000a39ab9761f1d97c68998c6003e
Markbear <41306229+Markbear@users.noreply.github.com>
2018-07-24 22:08:44 +0100

    Bootstrap styles added to search form

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
+	<div class="form-group">
+		<input type="text" class="search-field form-control" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" aria-describedby="search-form">
+	</div>
+	<button type="submit" class="btn btn-default" id="search-form"><?php echo esc_attr_x( 'Search', 'submit button' ) ?>
+	</button>
+</form>
