7b1208fee259e4da80d16e293a2d2d12e70fa7d6
Markbear <41306229+Markbear@users.noreply.github.com>
2018-07-24 21:53:19 +0100

    Bootstrap styles added to sidebar widget

--- inc/widgets.php
+++ inc/widgets.php
@@ -6,12 +6,12 @@
  */
 function strapcore_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'strapcore' ),
+		'name'          => esc_html__( 'Right Sidebar', 'strapcore' ),
 		'id'            => 'sidebar-1',
 		'description'   => esc_html__( 'Add widgets here.', 'strapcore' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
+		'before_widget' => '<section id="%1$s" class="widget card %2$s"><div class="card-body">',
+		'after_widget'  => '</div></section>',
+		'before_title'  => '<h2 class="widget-title card-title">',
 		'after_title'   => '</h2>',
 	) );
 }
